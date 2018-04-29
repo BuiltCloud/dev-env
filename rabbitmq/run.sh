d014936ec9ffbbda3a71ecd8eea4370e23271b21
keke <keke@cnblogs.com>
2018-04-30 07:08:43 +0800

    Expose other ports

--- rabbitmq/run.sh
+++ rabbitmq/run.sh
@@ -1,6 +1,9 @@
 docker run -d --hostname my-rabbit \
 --name rabbit \
 -p 5671:5671 \
+-p 5672:5672 \
+-p 4369:4369 \
+-p 25672:25672 \
 --network my-net \
 -v rabbit_volume:/var/lib/rabbitmq \
 rabbitmq:3
