b25f31b9295fe0531fadb3d6b0999c5c32469268
keke <keke@cnblogs.com>
2018-07-16 17:40:36 +0800

    Tweak mongo component

--- mongodb/scripts/setup.sh
+++ mongodb/scripts/setup.sh
@@ -1,36 +1,29 @@
 #!/bin/bash
-sleep 1
-mongo --host mongo1 <<EOF
-   var cfg = {
-  	"_id" : "my-mongo-set",
-  	"members" : [
-  		{
-  			"_id" : 0,
-  			"host" : "mongo1:27017",
-            "priority": 2
-  		},
-  		{
-  			"_id" : 1,
-  			"host" : "mongo2:27017",
-            "priority": 0
-  		},
-  		{
-  			"_id" : 2,
-  			"host" : "mongo3:27017",
-            "priority": 0
-  		}
-  	]
+sleep 10
+
+echo SETUP.sh time now: `date +"%T" `
+mongo --host mongo1:27017 <<EOF
+  var cfg = {
+    "_id": "my-mongo-set",
+    "version": 1,
+    "members": [
+      {
+        "_id": 0,
+        "host": "mongo1:27017",
+        "priority": 2
+      },
+      {
+        "_id": 1,
+        "host": "mongo2:27017",
+        "priority": 0
+      },
+      {
+        "_id": 2,
+        "host": "mongo3:27017",
+        "priority": 0
+      }
+    ]
   };
   rs.initiate(cfg, { force: true });
   rs.reconfig(cfg, { force: true });
-EOF 
-
-sleep 1
-mongo --host mongo2 <<EOF
-db.setSlaveOk();
-EOF
-
-sleep 1
-mongo --host mongo3 <<EOF
-db.setSlaveOk();
 EOF
